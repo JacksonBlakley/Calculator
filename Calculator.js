4084465eae39f89790ef7f27c5318609c49b468b
Jackson Blakley <jacksonblakley@gmail.com>
2020-04-22 20:46:46 -0500

    updates

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,6 @@
 var in_arr = "12/5*9+9.4*2";
+var input = [];
+var strX = []
 var x = 2;
 var y = 5;
 var op = "/";
@@ -40,6 +42,63 @@ function operate (x, y, operator) {
         return value;
     }
 }
+
+function listen() {
+    
+    const allbtn = document.querySelectorAll('.btn');
+    allbtn.forEach((div) => {
+    div.addEventListener('click', () => {
+    var x = div.id;
+    strX.push(x);
+    var cleanInput = strX.join('');
+    const display = document.querySelector('#display');
+    display.textContent = cleanInput;
+    input = cleanInput;
+    console.log('input = ' + input);
+    });
+    }); 
+
+    const allOperand = document.querySelectorAll('.operand');
+    allOperand.forEach((div) => {
+    div.addEventListener('click', () => {
+    var y = div.id;
+    strX.push(y);
+    var cleanInput = strX.join('');
+    display.textContent = cleanInput;
+    input = cleanInput
+    console.log('input = ' + input);
+    });
+    }); 
+
+
+
+}
+
+
+function clear () {
+    const clearbtn = document.querySelector('.clear');
+    clearbtn.addEventListener('click', () => {
+    input = [];
+    strX = [];
+    display.textContent = input;
+    console.log('input = ' + input);
+    })
+
+}
+
+function backspace () {
+    const backspace = document.querySelector('.backspace');
+    backspace.addEventListener('click', () => {
+    input = input.slice(0,-1);
+    display.textContent = input;
+    console.log('input = ' + input);
+    })
+}
+
+listen();
+clear();
+backspace ();
+
 
 var solution = operate(x, y, op);
 console.log("operate = " + solution);
